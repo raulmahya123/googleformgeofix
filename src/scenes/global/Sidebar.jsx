ad7a284b859c044585922ecf534385983b4d3db2
raulmahya123 <raulmahya11@gmail.com>
2022-10-28 19:09:21 +0700

    beres juga kmu

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -96,7 +96,7 @@ const Sidebar = () => {
                   alt="profile-user"
                   width="100px"
                   height="100px"
-                  src={`../../assets/user.png`}
+                  src={`../../assets/icons8-dino-67.png`}
                   style={{ cursor: "pointer", borderRadius: "50%" }}
                 />
               </Box>
@@ -107,10 +107,10 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Ed Roh
+                  SELAMAT DATANG
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
-                  VP Fancy Admin
+                  ADMIN MUSEUM GEOLOGI
                 </Typography>
               </Box>
             </Box>
